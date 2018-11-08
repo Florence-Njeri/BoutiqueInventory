8ca3a160d2aae3a7e53280758d52aa715c547955
Wise4rmGod <wise4rmgod@gmail.com>
2018-11-08 11:17:38 +0100

    fix the issue of not saving to firebase firestore database..

--- app/src/main/java/com/example/admin/boutiqueinventory/AddNewInventory.java
+++ app/src/main/java/com/example/admin/boutiqueinventory/AddNewInventory.java
@@ -2,56 +2,97 @@ package com.example.admin.boutiqueinventory;
 
 import android.annotation.TargetApi;
 import android.os.Build;
+import android.support.annotation.NonNull;
 import android.support.design.widget.TextInputEditText;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Objects;
 
+import butterknife.BindView;
+
 public class AddNewInventory extends AppCompatActivity {
-    private FirebaseDatabase firebaseDatabase;
-    private DatabaseReference databaseReference;
-    TextInputEditText itemName, colorNumber, quantity, price;
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private static final String TAG= "Tag";
+    @BindView(R.id.item_name)
+    EditText itemName;
+    @BindView(R.id.color_number_edit_text)
+    EditText colorNumber;
+    @BindView(R.id.quantity)
+    EditText quantity;
+    @BindView(R.id.price)
+    EditText price;
+    @BindView(R.id.add_new_inventory)
+    Button addNewInventory;
+
+ //   TextInputEditText itemName, colorNumber, quantity, price;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_new_inventory);
 
-        firebaseDatabase = FirebaseDatabase.getInstance();
-        databaseReference = firebaseDatabase.getReference("inventory");
-
-        //Instantiate the EditText views
-        itemName = findViewById(R.id.item_name);
-        colorNumber = findViewById(R.id.color_number_edit_text);
-        quantity = findViewById(R.id.quantity);
-        price = findViewById(R.id.price);
-
         //OnClick listener for the submit Button
 
-        Button addNewInventory = findViewById(R.id.add_new_inventory);
         addNewInventory.setOnClickListener(new View.OnClickListener() {
             @TargetApi(Build.VERSION_CODES.KITKAT)
             @Override
             public void onClick(View v) {
-                String name = Objects.requireNonNull(itemName.getText()).toString();
-                String color = Objects.requireNonNull(colorNumber.getText()).toString();
-                String quantityEntered = Objects.requireNonNull(quantity.getText()).toString();
-                String priceItem = Objects.requireNonNull(price.getText()).toString();
 
-                //Instantiate the DisplayItem class for the data typed in by app user
-                DisplayItems displayItems = new DisplayItems(name, Integer.parseInt(color), Integer.parseInt(quantityEntered), Integer.parseInt(priceItem));
 
-                databaseReference.setValue(displayItems);
-                Toast.makeText(AddNewInventory.this, "Information Saved!!", Toast.LENGTH_SHORT).show();
+                if (itemName.getText().toString().isEmpty() && colorNumber.getText().toString().isEmpty()
+                        && quantity.getText().toString().isEmpty() && price.getText().toString().isEmpty()) {
+
+                    toast("Please type something some fields are empty");
+                }
+                else {
+
+                    // Create a new user
+                    Map<String, Object> user = new HashMap<>();
+                    user.put("itemname", itemName.getText().toString().trim());
+                    user.put("colornumber", colorNumber.getText().toString().trim());
+                    user.put("quantity", quantity.getText().toString().trim());
+                    user.put("price", price.getText().toString().trim());
+
+                      // Add a new document with a generated ID From firestore
+                    db.collection("Inventorysystem")
+                            .add(user)
+                            .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                                @Override
+                                public void onSuccess(DocumentReference documentReference) {
+                                    Log.d(TAG, "DocumentSnapshot added with ID: " + documentReference.getId());
+                                    toast("Information Saved!!");
+                                }
+                            })
+                            .addOnFailureListener(new OnFailureListener() {
+                                @Override
+                                public void onFailure(@NonNull Exception e) {
+                                    Log.w(TAG, "Error adding document", e);
+                                }
+                            });
+
+
+                }
             }
         });
+    }
+
+    private void toast(String string){
+        Toast.makeText(AddNewInventory.this, string, Toast.LENGTH_SHORT).show();
     }
 
 }
